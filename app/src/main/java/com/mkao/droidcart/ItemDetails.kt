693179b9f11ec65a05d86d3b676b83f21b78fed0
mkao_dev <mkaoibrahim@gmail.com>
2022-04-19 11:17:26 +0300

    ViewModels Updated

--- app/src/main/java/com/mkao/droidcart/ItemDetails.kt
+++ app/src/main/java/com/mkao/droidcart/ItemDetails.kt
@@ -2,10 +2,36 @@ package com.mkao.droidcart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.mkao.droidcart.database.Item
+import com.mkao.droidcart.databinding.ItemDetailsBinding
 
-class ItemDetails : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.item_details)
+class ItemDetails : Fragment() {
+    private val navigationArgs :ItemDetails by navigationArgs()
+    lateinit var item: Item
+
+    private val viewModel: StockViewModel by activityViewModels {
+        InventoryViewModelFactory(
+            (activity?.application as StockApplication).database.itemDao()
+        )
     }
+
+    private var _binding : ItemDetailsBinding? = null
+    private val  binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+        _binding = ItemDetailsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+
 }
