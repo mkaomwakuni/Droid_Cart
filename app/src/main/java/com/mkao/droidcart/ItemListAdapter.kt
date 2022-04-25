21bfa607cca38568dbc18ed6957989aa2e90e382
mkao_dev <mkaoibrahim@gmail.com>
2022-04-25 17:11:47 +0300

    Fragments

--- app/src/main/java/com/mkao/droidcart/ItemListAdapter.kt
+++ app/src/main/java/com/mkao/droidcart/ItemListAdapter.kt
@@ -2,12 +2,58 @@ package com.mkao.droidcart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
 import com.mkao.droidcart.database.Item
+import com.mkao.droidcart.database.getFormattedPrice
+import com.mkao.droidcart.databinding.ItemListItemBinding
 
 //This adapter implements for recyclerView
-class ItemListAdapter() : ListAdapter<>(DiffCallback){
+class ItemListAdapter(private val onItemClicked:(Item)->Unit) :
+    ListAdapter<Item,ItemListAdapter.ItemViewHolder>(DiffCallback){
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            ItemListItemBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                )
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.itemView.setOnClickListener {
+            onItemClicked(current)
+        }
+        holder.bind(current)
+    }
+
+    class ItemViewHolder(private var binding: ItemListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Item) {
+            binding.itemName.text = item.itemName
+            binding.itemPrice.text = item.getFormattedPrice()
+            binding.itemQuantity.text = item.itemQuantity.toString()
+        }
+    }
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Item>() {
+            override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+                return oldItem.itemName == newItem.itemName
+            }
+        }
+    }
 
 }
 
--- app/src/main/java/com/mkao/droidcart/ItemListFragment.kt
+++ app/src/main/java/com/mkao/droidcart/ItemListFragment.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.mkao.droidcart.databinding.ItemListFragmentBinding
 
 class ItemListFragment : Fragment() {
@@ -32,9 +34,25 @@ class ItemListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter = ItemListAdapter{
-            val action = ItemListFragmentDirections
-        }
-    }
+        val adapter = ItemListAdapter {
+            val action = ItemListFragmentDirections.itemListFragment_to_item_add_fragment{
+                this.findNavController().navigate(action)
+            }
+        binding.recyclerView.layoutManager = LinearLayout(this.context)
+        binding.recyclerView.adapter= adapter
 
+        //Attach an observer on allItems list to update the UI automatically
+        //changes
+        viewModel.allItems.observe(this.viewLifecycleOwner){ items->
+            items.let {
+                adapter.submitList(it)
+            }
+        }
+        binding.floatingbtn.setOnClickListener {
+            val action = ItemListFragmentDirections.action_itemDetailFragment_to_addItemFragment
+            (getString(R.string.add_fragment_title)
+                    )
+                    this.findNavController().navigate(action)
+        }
+      }
     }
